9088c2d9f5903c027d67d7aaba0b0ea124eb56e2
Админ <kustanaev.tamirlan@gmail.com>
2024-07-11 19:06:41 +0600

    feat. Timati

--- src/routes/excel/excel-service.ts
+++ src/routes/excel/excel-service.ts
@@ -2,6 +2,77 @@ import { read, utils } from 'xlsx'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import * as fs from 'fs'
 
+const mission = `# Миссия, культура
+
+      ## Миссия
+
+      Подготовить 10,000 высококлассных разработчиков и поставить Казахстан на технологическую карту мира, когда сотни миллионов людей по всему миру используют софтверные продукты “MADE IN KAZAKHSTAN”.
+
+      Train 10,000 high-class software engineers and build software used by millions around the globe. 
+
+      ## Ценности
+
+      1. Получать удовольствие
+      2. Распространять эмпатию 
+      3. Создавать ценность
+      4. Творить историю
+
+      1. Have fun 
+      2. Spread empathy 
+      3. Generate value 
+      4. Make history
+
+      ## Культура (студенты)
+
+      1. Have fun
+          1. Получайте удовольствие
+      2. Not knowing something is okay
+          1. Что-то не знать это окей
+      3. Not understanding something is okay
+          1. Что-то не понимать это окей
+      4. Not “performing” is okay
+          1. Что-то не успевать это окей
+      5. Be mentor yourself, help your peers
+          1. Будь ментором, помогай другим
+      6. Help someone
+          1. Помоги кому-нибудь
+      7. Be open
+          1. Оставайся открытым
+      8. Just google it
+          1. Просто загугли
+      9. Listen, Learn, Repeat
+          1. Слушай, Запоминай, Повторяй
+      10. Always Be Coding
+          1. Всегда Пиши Код
+      11. First try to google, then ask for help
+          1. Сначала попробуй загуглить, потом проси помощи
+      12. Learn what not to ask
+          1. Научись понимать какие вопросы не задавать
+      13. Done is better than perfect 
+          1. Сделано лучше, чем идеально
+      14. Release something today
+          1. Зарелизь что-нибудь сегодня
+      15. Release early, ship often
+          1. Релизь пораньше, обновляй почаще
+      16. Don’t waste time solving problems you don’t yet have
+          1. Не тратьте время решая проблемы, которых у вас еще нет
+      17. Stay creative
+          1. Будьте креативными
+      18. Always ask yourself “What problem am I solving?”
+          1. Всегда спрашивай себя “Какую проблему решаю?”
+      19. Always be curious
+          1. Будьте любознательны
+      20. Get things to the finish line
+          1. Доводите начатое до конца
+      21. Don’t forget to invent 
+          1. Не забывайте изобретать
+      22. Think big
+          1. Думайте масштабно
+      23. Have a bias for action
+          1. Делать > Говорить
+          дайте короткий ответ.
+    `
+
 class ExcelService {
   private genAI: any
 
@@ -60,8 +131,16 @@ class ExcelService {
   }
 
   private async sendToGemini(data: any): Promise<string> {
-    const promptText = `Please analyze the following user data and decide whether the participant should be accepted into the programming school. give me short answer. Be not strict in your evaluation: 
-    ${JSON.stringify(data)}`
+    const promptText = `Проанализируйте следующие данные пользователя и решите, стоит ли принимать участника в стартап AI инкубатор из Казахстана который находится в Алмате. 
+    Наша компания называется Nfactorial и каждое лето мы делаем инкубатор в Сатпаевском Университете
+    
+    наша миссия:
+    ${mission}
+    
+    желательно брать людей у которых уже есть опыт в программирование вебсайтов (уже использовал какие либо веб-фреймворки)
+    отвечай мне с причинами почему да или почему нет коротко.
+    ${JSON.stringify(data)}
+    `
 
     try {
       const model = this.genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
